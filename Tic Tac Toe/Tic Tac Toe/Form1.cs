1d01f22e87b052dcc273f2e7978920d4fd0ceafe
Darian Fisher <darian.edward.fisher@outlook.com>
2020-02-06 10:18:18 -0500

    Win condition enabled
    Completed funcationality for two human players

--- Tic Tac Toe/Tic Tac Toe/Form1.cs
+++ Tic Tac Toe/Tic Tac Toe/Form1.cs
@@ -12,88 +12,86 @@ namespace Tic_Tac_Toe
 {
     public partial class Form1 : Form
     {
+        int turn = 0;
         public Form1()
         {
             InitializeComponent();
-            int turn = 0;
         }
 
 
-        private void A0_Click(object sender, EventArgs e)
+        private void Button_Click(object sender, EventArgs e)
         {
-            if (A0.Text == "Press To Select")
+            Button button = (Button)sender; //Takes the Button which was sent to the method as a generic object and casts it back to a Button. Makes it so only one event is necessary.
+            if (button.Text == "Press To Select")
             {
-                A0.Text = "X";
+                if (turn % 2 == 0)
+                {
+                    button.Text = "X";
+                }
+                else
+                {
+                    button.Text = "O";
+                }
+                WinCheck();
+                turn++;
             }
         }
-
-        private void A1_Click(object sender, EventArgs e)
-        {
-            if (A1.Text == "Press To Select")
-            {
-                A1.Text = "X";
-            }
-        }
-
-        private void A2_Click(object sender, EventArgs e)
-        {
-            if (A2.Text == "Press To Select")
-            {
-                A2.Text = "X";
-            }
-        }
-
-        private void B0_Click(object sender, EventArgs e)
-        {
-            if (B0.Text == "Press To Select")
-            {
-                B0.Text = "X";
-            }
-        }
-
-        private void B1_Click(object sender, EventArgs e)
-        {
-            if (B1.Text == "Press To Select")
-            {
-                B1.Text = "X";
-            }
-        }
-
-        private void B2_Click(object sender, EventArgs e)
-        {
-            if (B2.Text == "Press To Select")
-            {
-                B2.Text = "X";
-            }
-        }
-
-        private void C0_Click(object sender, EventArgs e)
-        {
-            if (C0.Text == "Press To Select")
-            {
-                C0.Text = "X";
-            }
-        }
-
-        private void C1_Click(object sender, EventArgs e)
-        {
-            if (C1.Text == "Press To Select")
-            {
-                C1.Text = "X";
-            }
-        }
-
-        private void C2_Click(object sender, EventArgs e)
-        {
-            if (C2.Text == "Press To Select")
-            {
-                C2.Text = "X";
-            }
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void WinCheck()
+        {
+            bool winState = false;
+            //Horizontal Checks
+            if (A0.Text == A1.Text && A1.Text == A2.Text && A0.Text != "Press To Select") winState = true;
+            if (B0.Text == B1.Text && B1.Text == B2.Text && B0.Text != "Press To Select") winState = true;
+            if (C0.Text == C1.Text && C1.Text == C2.Text && C0.Text != "Press To Select") winState = true;
+            //Vertical Checks
+            if (A0.Text == B0.Text && B0.Text == C0.Text && A0.Text != "Press To Select") winState = true;
+            if (A1.Text == B1.Text && B1.Text == C1.Text && A1.Text != "Press To Select") winState = true;
+            if (A2.Text == B2.Text && B2.Text == C2.Text && A2.Text != "Press To Select") winState = true;
+            //Diagonal Checks
+            if (A0.Text == B1.Text && B1.Text == C2.Text && A0.Text != "Press To Select") winState = true;
+            if (A2.Text == B1.Text && B1.Text == C0.Text && A2.Text != "Press To Select") winState = true;
+
+
+            if (winState == true)
+            {
+                if (turn % 2 == 0)
+                {
+                    MessageBox.Show("Xs wins the game!");
+                }
+                else
+                {
+                    MessageBox.Show("Os wins the game!");
+                }
+                try
+                {
+                    foreach (Control control in Controls)
+                    {
+                        Button button = (Button)control;
+                        button.Enabled = false; //Disables all buttons after a winner is declared. Need to set to true when starting new game.
+                    }
+                }
+                catch { }
+            }
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Written by Darian Fisher, Jackson Foster, and Logan Jones", "About Tic Tac Toe");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
